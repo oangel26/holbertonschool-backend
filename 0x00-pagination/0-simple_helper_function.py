fff52dd0d37411cfe34b713c772c1afe5390be34
oangel26 <oa.angel26@gmail.com>
2022-08-20 11:29:07 -0500

    first commit

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    Method that takes page and page_size args and returns tuple
+    containing start_index and end_index corresponding to the range
+    of indexes to return in a list for those particular pagination
+    parameters
+    """
+    end_index = page * page_size
+    start_index = end_index - page_size
+    return (start_index, end_index)
+
+
+if __name__ == "__main__":
+    res = index_range(1, 7)
+    print(type(res))
+    print(res)
+
+    res = index_range(page=3, page_size=15)
+    print(type(res))
+    print(res)
