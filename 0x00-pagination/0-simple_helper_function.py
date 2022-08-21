d96f8d7a06c824cae60057105bc99671f6b96c12
oangel26 <oa.angel26@gmail.com>
2022-08-21 06:57:22 -0500

    pycodestyle blank line fixed

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@ Simple helper function
 """
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Method that takes page and page_size args and returns tuple
